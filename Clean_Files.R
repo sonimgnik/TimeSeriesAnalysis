b11dfdc8bd1a24f9c9583b1983e6691e386db5c3
Nick Toscano <ntoscano01@gmail.com>
2020-01-19 18:50:41 -0500

    Create Clean_Files.R

---
+++ Clean_Files.R
@@ -0,0 +1,13 @@
+#Clean files for new session
+
+#List files in directory
+list.files()
+
+# delete another file
+unlink("time_series.jpg")
+
+# delete another file
+#file.remove("ticker_data.csv")
+
+#List files in directory
+list.files()
